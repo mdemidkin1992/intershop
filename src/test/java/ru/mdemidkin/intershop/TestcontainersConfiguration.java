4f9b111606e4b6e6eddd882b0050f100ae6cea7c
Maxim Demidkin <mdemidkin@yandex.ru>
2025-05-13 16:36:01 +0200

    настройка проекта

---
+++ src/test/java/ru/mdemidkin/intershop/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package ru.mdemidkin.intershop;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
