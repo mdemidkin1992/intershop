37df8fc17fa931b0b58ae9a1209a4a5962efcebc
Maxim Demidkin <mdemidkin@yandex.ru>
2025-05-16 12:02:07 +0200

    добавить README.md файл, исправить ddl миграции на xml формат с preconditions и rollback секциями

--- src/test/java/ru/mdemidkin/intershop/config/PostgresTestcontainersConfig.java
+++ src/test/java/ru/mdemidkin/intershop/config/PostgresTestcontainersConfig.java
@@ -1,12 +1,10 @@
 package ru.mdemidkin.intershop.config;
 
 import org.junit.jupiter.api.BeforeAll;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 
-@TestConfiguration(proxyBeanMethods = false)
 public class PostgresTestcontainersConfig {
 
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16.1-alpine")
